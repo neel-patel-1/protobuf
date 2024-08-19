2bd1b14c0f6d09713ea3c6ac5aaaab15272448db
Neel Patel <neelpatelbiz@gmail.com>
2024-08-19 23:45:00 +0000

    bc cmake

---
+++ cmake/libprotobuf_bc.cmake
@@ -0,0 +1,14 @@
+# CMake definitions for libprotobuf (the "full" C++ protobuf runtime).
+
+include(${protobuf_SOURCE_DIR}/src/file_lists.cmake)
+include(${protobuf_SOURCE_DIR}/cmake/protobuf-configure-target.cmake)
+
+add_library(libprotobuf_pass ${protobuf_SHARED_OR_STATIC}
+  ${libprotobuf_srcs}
+  ${libprotobuf_hdrs}
+  ${protobuf_version_rc_file})
+
+set_target_properties(libprotobuf_pass PROPERTIES LINKER_LANGUAGE CXX)
+
+llvmir_attach_bc_target( TARGET libprotobuf_pass_bc DEPENDS libprotobuf_pass )
+llvmir_attach_opt_pass_target( TARGET libprotobuf_pass_bc_opt DEPENDS libprotobuf_pass_bc -mem2reg )
